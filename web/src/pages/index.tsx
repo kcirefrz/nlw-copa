dc83c8ab98701b82ed55d846fd065b9801fdea00
Erick <erickfaresrvz@gmail.com>
2022-11-03 02:45:42 -0300

    docs: axios and axios routes

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,13 +4,38 @@ import logoImg from "../assets/logo.svg";
 import usersAvatarExampleImg from "../assets/users-avatar-example.png";
 import iconCheckImg from "../assets/icon-check.svg";
 import { api } from "../lib/axios";
+import { FormEvent, useState } from "react";
 
 interface HomeProps {
   poolCount: number;
   guessCount: number;
+  userCount: number;
 }
 
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState("");
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post("/pools", {
+        title: poolTitle,
+      });
+
+      const { code } = response.data;
+
+      await navigator.clipboard.writeText(code);
+
+      alert("Pool successfully created, code copied to the transfer area!");
+
+      setPoolTitle("");
+    } catch (err) {
+      console.log(err);
+      alert("Error when creating pool, try again!");
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto gap-28 grid grid-cols-2 items-center">
       <main>
@@ -24,17 +49,19 @@ export default function Home(props: HomeProps) {
           <Image src={usersAvatarExampleImg} alt="" />
 
           <strong className="text-gray-100 text-xl">
-            <span className="text-ignite-500">12.592 people</span> already
-            using!
+            <span className="text-ignite-500">{props.userCount} people</span>{" "}
+            already using!
           </strong>
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
           <input
-            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
             type="text"
             required
             placeholder="What's yours pool's name?"
+            onChange={(event) => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button
             className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
@@ -80,15 +107,18 @@ export default function Home(props: HomeProps) {
 }
 
 export const getServerSideProps = async () => {
-  const [poolCountResponse, guessCountResponse] = await Promise.all([
-    api.get("pools/count"),
-    api.get("guesses/count"),
-  ]);
+  const [poolCountResponse, guessCountResponse, userCountResponse] =
+    await Promise.all([
+      api.get("pools/count"),
+      api.get("guesses/count"),
+      api.get("users/count"),
+    ]);
 
   return {
     props: {
       poolCount: poolCountResponse.data.count,
       guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
     },
   };
 };
